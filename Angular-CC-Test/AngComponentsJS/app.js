b4fdad4607d6f71e3a54cc557bb76f1ec15ba54f
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2017-11-09 21:52:27 -0500

    update

--- Angular-CC-Test/AngComponentsJS/app.js
+++ Angular-CC-Test/AngComponentsJS/app.js
@@ -1,2 +1,19 @@
-angular.module('myApp', ['ngRoute', 'ui.router']);
-  
+angular.module('myApp', ['ngRoute', 'ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('home', {
+                    url:'/home',
+                    templateUrl: 'Components/home.html',
+                    controller: 'tableDataDisplay'
+                })
+
+                .state('formatted', {
+                    url:'/formatted',
+                    templateUrl: 'Components/category.html',
+                    controller: 'tableDataDisplay'
+                })
+    
+                $urlRouterProvider.otherwise('/home');
+
+        }]);
